d75f4d598e5e2d5a9eec9c34a3094da82020e0dd
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-05-24 18:36:35 +0200

    Setup

--- Main.py
+++ Main.py
@@ -11,15 +11,15 @@ def ask(polish, spanish):
     UserInput = ""
     # for word in polish_words:
 
-    UserInput = input("Jak jest " + polish_words[randNum] + " po hiszpańsku?: ")
+    UserInput = input("Jak jest " + polish[randNum] + " po hiszpańsku?: ")
     UserInput.strip().replace(" ", "")
     UserInput = UserInput.lower()
     #print(UserInput)
 
-    if UserInput == spanish_words[randNum]:
+    if UserInput == spanish[randNum]:
         print("Muy bien!")
     else:
-        print("Fatal! Prawidłowa odpowiedź to " + spanish_words[randNum])
+        print("Fatal! Prawidłowa odpowiedź to " + spanish[randNum])
 
     polish.pop(randNum)
     spanish.pop(randNum)
@@ -29,28 +29,32 @@ def ask(polish, spanish):
         print("Nie ma więcej słówkek!")
         input("")
 
+def main():
 
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
 
-textfile = open(filename, "r+", encoding="utf-8")
+    textfile = open(filename, "r+", encoding="utf-8")
 
-polish_words = []
-spanish_words = []
+    polish_words = []
+    spanish_words = []
 
-for line in textfile:
-    langSplit = line.strip().replace(" ", "").lower().split(":")
-    #print(langSplit)
-    #print(langSplit)
-    polish_words.append(langSplit[0])
-    spanish_words.append(langSplit[1])
+    for line in textfile:
+        langSplit = line.strip().replace(" ", "").lower().split(":")
+        #print(langSplit)
+        #print(langSplit)
+        polish_words.append(langSplit[0])
+        spanish_words.append(langSplit[1])
 
-#print(polish_words)
-#print(spanish_words)
+    #print(polish_words)
+    #print(spanish_words)
 
-ask(polish_words, spanish_words)
+    ask(polish_words, spanish_words)
 
-textfile.close()
+    textfile.close()
 
 
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
