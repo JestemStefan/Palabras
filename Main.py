818532f1586dae96b14997484a209fdc69ba799b
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-06-14 23:35:33 +0200

    Added: - requested spanish letters at the beginning - Selecting only txt files - discarding words with no or wrong translation

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ def ask(polish, spanish):
     # for word in polish_words:
 
     UserInput = input("Jak jest " + polish[randNum] + " po hiszpańsku?: ")
-    UserInput = UserInput.strip().replace(" ", "")
+    UserInput = UserInput.strip().replace("  ", " ")
     UserInput = UserInput.lower()
     #print(UserInput)
 
@@ -33,32 +33,36 @@ def main():
 
 
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    filename = askopenfilename(title="Wybierz plik ze słówkami", filetypes = (("Plik tekstowy","*.txt"),("Wszystkie pliki","*.*")))  # show an "Open" dialog box and return the path to the selected file
 
     textfile = open(filename, "r+", encoding="utf-8")
 
     polish_words = []
     spanish_words = []
 
-    print("Naciśnij dowolny klawisz aby rozpocząć!")
+    print("Hiszpańskie litery: á, é, í, ó, ú, ü, ñ, ¿, ¡ \n")
+    #print("Naciśnij dowolny klawisz aby rozpocząć!")
     for line in textfile:
-        langSplit = line.strip().replace(" ", "").lower().split(":")
+        langSplit = line.strip().replace("  ", " ").lower().split(":")
 
         if len(langSplit) == 2:
-            if len(langSplit[0]) and len(langSplit[1]):
+            if len(langSplit[1]) and len(langSplit[0]):
 
-                #print(len(langSplit[1]))
-                polish_words.append(langSplit[0])
-                spanish_words.append(langSplit[1])
+                polish_words.append(langSplit[1])
+                spanish_words.append(langSplit[0])
 
     #print(polish_words)
     #print(spanish_words)
 
-    ask(polish_words, spanish_words)
+    if len(polish_words) > 0 and len(spanish_words) > 0:
+        ask(polish_words, spanish_words)
+    else:
+        print("Brak słówek, Wybierz odpowiedni plik txt!")
 
     textfile.close()
 
 
-if __name__ == "__main__":
-    # execute only if run as a script
-    main()
+#if __name__ == "__main__":
+    #main()
+
+main()
