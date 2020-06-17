12487e2a396049eff1d694dbf7df2f44edac3874
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2020-06-17 20:35:39 +0200

    Added: - Only 25 words are randomly selected each time

--- Main.py
+++ Main.py
@@ -1,46 +1,14 @@
 from __future__ import unicode_literals
 from tkinter.filedialog import askopenfilename
 from tkinter import *
+import random as rng
 
 words = []
-wordsLoaded = False
-tableDone = False
-
-def make_tables(root, word):
-    entries = []
-    i = 0
-
-    for w in word:
-        Label(root, text=w, width=15, anchor='center').grid(row=i)
-        ent = Entry(root, justify='center')
-        ent.grid(row=i, column=1, padx=5, pady=5)
-        entries.append(ent)
-        i += 1
-    return entries
-
-
-def check_words(entries, root, spanish_words):
-
-    for i, entry in enumerate(entries):
-        text = entry.get()
-
-        text = text.strip().replace("  ", " ")
-        text = text.lower()
-        if text == spanish_words[i]:
-            entry.configure({"background": "#caffab", "font": "Times 10 normal"})
-        else:
-            entry.configure({"background": "#ffbaab", "font": "Times 10 overstrike"})
-            Label(root, text=spanish_words[i], width=15, anchor='center', bg="#caffab").grid(column=3, row=i)
-
-        # clear user input
-        entry.delete(0, END)
-        entry.insert(0, text)
 
 
 def get_words(button, root):
 
     global words
-    global wordsLoaded
 
     polish_words = []
     spanish_words = []
@@ -64,19 +32,67 @@ def get_words(button, root):
     textfile.close()
 
     if len(polish_words) > 0 and len(spanish_words) > 0:
+
+        # randomize words
+        z = list(zip(spanish_words, polish_words))
+        rng.shuffle(z)
+        z = z[:25]
+        print(z)
+
+        spanish_words[:], polish_words[:] = zip(*z)
+
         words = [spanish_words, polish_words]
-        wordsLoaded = True
 
         ents = make_tables(root, words[1])
         b2 = Button(root, text='Sprawdź!', command=(lambda e=ents: check_words(e, root, words[0])))
         b2.grid(row=len(words[1]), column=1)
 
-        root.geometry("350x750")
+        #root.geometry("450x750")
+        root.geometry("")
 
     else:
         print("Brak słówek, Wybierz odpowiedni plik txt!")
 
     button.destroy()
+
+
+
+def make_tables(root, word):
+    entries = []
+    i = 0
+
+    for w in word:
+        Label(root, text=w, width=len(w), anchor='center').grid(row=i)
+        ent = Entry(root, justify='center')
+        ent.grid(row=i, column=1, padx=5, pady=5)
+        entries.append(ent)
+        i += 1
+    return entries
+
+
+def check_words(entries, root, spanish_words):
+
+    longest_word = max(spanish_words, key=len)
+
+    for i, entry in enumerate(entries):
+        text = entry.get()
+
+        text = text.strip().replace("  ", " ")
+        text = text.lower()
+        if text == spanish_words[i]:
+            entry.configure({"background": "#caffab", "font": "Times 10 normal"})
+        elif text == "":
+            entry.configure({"background": "#ffbaab", "font": "Times 10 normal"})
+            Label(root, text=spanish_words[i], width=len(longest_word), anchor='center', bg="#caffab").grid(column=3, row=i)
+        else:
+            entry.configure({"background": "#ffbaab", "font": "Times 10 overstrike"})
+            Label(root, text=spanish_words[i], width=len(longest_word), anchor='center', bg="#caffab").grid(column=3, row=i)
+
+        # clear user input
+        entry.delete(0, END)
+        entry.insert(0, text)
+
+
 
 def quit(r):
     r.destroy()
